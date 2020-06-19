755689f64ec0d238d17cc5f501a129907cdfb19f
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-06-19 09:55:07 +0530

    Express simple REST with Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -2,12 +2,32 @@ const express = require('express')
 const http = require('http');
 const { Console } = require('console');
 const morgan = require('morgan');
+const bodyparser = require("body-parser")
 
+const dishRouter = require("./routes/dishRouter");
 const hostname='localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyparser.json());
+app.use('/dishes',dishRouter);
+
+
+
+app.get('/dishes/:dishId',(req,res,next)=>{
+    res.end("Will send details of the dish" + req.params.dishId+' to you !');
+});
+app.post('/dishes/:dishId',(req,res,next) => {
+    res.end("POST operation not supported on /dishes/"+req.params.dishId);
+});
+app.put('/dishes/:dishId',(req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name +' with details: '+ req.body.description);
+});
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 app.use(express.static(__dirname+'/public'));
 
