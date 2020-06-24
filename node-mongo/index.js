ac52490a014cf1a22075625c43f56bcb4535c1a9
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-06-24 22:42:10 +0530

    sdasd

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,33 +5,39 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = "conFusion";
 
-MongoClient.connect(url, (err,client) => {
-    assert.equal(err,null);
-
+MongoClient.connect(url).then((client) => {
     console.log("Connected correctly to server");
 
     const db = client.db(dbname);
     const collection = db.collection('dishes');
    
-    dboper.insertDocument(db, {"name":"Vadonut","description":"test"}, "dishes", (result) => {
+    dboper.insertDocument(db, {"name":"Vadonut","description":"test"}, "dishes")
+    .then((result) => {
+
         console.log("Insert Document: \n",result.ops);
 
-        dboper.findDocuments(db, "dishes", (docs) => {
+        return dboper.findDocuments(db, "dishes")
+    })
+        .then((docs) => {
             console.log("Found Documents: \n",docs);
 
-            dboper.updateDocument(db, {"name":"Vadonut"}, {"description":"Updated test"}, "dishes", (result) => {
+            return dboper.updateDocument(db, {"name":"Vadonut"}, {"description":"Updated test"}, "dishes")
+        })
+        .then((result) => {
                 console.log("Updated document: \n",result.result);
 
-                dboper.findDocuments(db, "dishes", (docs) =>{
+                return dboper.findDocuments(db, "dishes")
+        })
+        .then((docs) =>{
                     console.log("Found Updated documents: \n",docs);
 
-                    db.dropCollection("dishes", (result)=>{
+            return db.dropCollection("dishes")
+        })
+        .then((result)=>{
                         console.log("Dropeed Collection: ",result);
 
                         client.close();
-                    });
-                });
-            });
-        });
-    });
-});
+    })
+    .catch((err) => console.log(err));                
+})
+.catch((err) => console.log(err));
